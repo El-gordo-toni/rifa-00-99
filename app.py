5b1ac87539229f45a0eb462b464bbaea004876af
El-gordo-toni <gargoladefuego42@gmail.com>
2026-03-25 21:20:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ ADMIN_VIEW_KEY = os.environ.get("ADMIN_VIEW_KEY", "")  # ver panel admin
 
 # --- Datos de la rifa ---
 RAFFLE_TITLE = os.environ.get("RAFFLE_TITLE", "Rifa Fin de Año")
-RAFFLE_PRICE = os.environ.get("RAFFLE_PRICE", "Primer premio: Bolsa de Golf Wilson, Segundo Premio: Termo Stanley, Tercer Premio: Jarra Stanley")
+RAFFLE_PRICE = os.environ.get("RAFFLE_PRICE", "Primer premio: Bolsa de Golf Wilson, Segundo Premio: Drive R-1 ")
 RAFFLE_DATE  = os.environ.get("RAFFLE_DATE",  "Valor 10 Mil pesos, Se sortea al venderse todos los numeros")
 BANK_INFO = os.environ.get("BANK_INFO", "").strip()
 
